a1d618ed9c52f1d1e5cf9a84e867a6cef5b4aacf
Thooral <alejandro.toral.12@gmail.com>
2025-04-06 13:12:20 -0500

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Github, Linkedin, Mail, Menu, X, Moon, Sun } from 'lucide-react';
+import { Github, Linkedin, MessageCircleMore, Menu, X, Moon, Sun } from 'lucide-react';
 import { useState, useEffect } from 'react';
 
 const projects = [
@@ -191,7 +191,7 @@ function App() {
               href="mailto:your.email@example.com"
               className={`p-4 ${darkMode ? 'bg-gray-800/50 text-gray-300 hover:text-white hover:bg-gray-700/80' : 'bg-white/50 text-gray-600 hover:text-gray-900 hover:bg-white/80'} backdrop-blur-sm rounded-full transition-all duration-300 transform hover:scale-110 hover:shadow-lg`}
             >
-              <Mail size={24} />
+              <MessageCircleMore  size={24} />
             </a>
           </div>
         </div>
